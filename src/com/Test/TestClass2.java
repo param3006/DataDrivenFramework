f0082f5db5728781a72905745c8828e402a53aca
param3006 <parampreetsingh.p@outlook.com>
2023-12-30 13:47:33 +0530

    rebased

--- src/com/Test/TestClass2.java
+++ src/com/Test/TestClass2.java
@@ -6,7 +6,7 @@ public class TestClass2 {
 		// TODO Auto-generated method stub
 		System.out.println("Test");
 		System.out.println("Terst2");
-		System.out.println("Test8");
+		System.out.println("Test4");
 	}
 
 }

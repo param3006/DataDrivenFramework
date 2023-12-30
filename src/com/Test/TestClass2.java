19bf67f33425601523f9994c27a2640ef5978dd6
param3006 <parampreetsingh.p@outlook.com>
2023-12-30 13:50:17 +0530

    test5

--- src/com/Test/TestClass2.java
+++ src/com/Test/TestClass2.java
@@ -6,7 +6,7 @@ public class TestClass2 {
 		// TODO Auto-generated method stub
 		System.out.println("Test");
 		System.out.println("Terst2");
-		System.out.println("Tfest4");
+		System.out.println("Tfest5");
 	}
 
 }

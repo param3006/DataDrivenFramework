d8064b8575ae5633d1c923efa05ba26f9f5f87d6
param3006 <parampreetsingh.p@outlook.com>
2023-12-30 13:42:23 +0530

    test1

---
+++ src/com/Test/TestClass2.java
@@ -0,0 +1,12 @@
+package com.Test;
+
+public class TestClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+		System.out.println("Terst2");
+		System.out.println("Test8");
+	}
+
+}

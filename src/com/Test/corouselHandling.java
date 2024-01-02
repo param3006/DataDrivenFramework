1c7b6fbac8a87237d89e49043a80ebafba39fb5a
param3006 <parampreetsingh.p@outlook.com>
2024-01-02 21:28:27 +0530

    removed unused imports

--- src/com/Test/corouselHandling.java
+++ src/com/Test/corouselHandling.java
@@ -4,20 +4,17 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 
 import java.util.ArrayList;
-import java.util.Collection;
+
 import java.util.Collections;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.concurrent.TimeUnit;
+
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
+
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 
